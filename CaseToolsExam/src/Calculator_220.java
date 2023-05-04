3eb3322e93b261a3c2fe81a5f1e08a300af7fefe
exam <exam@Bcalab4-m19>
2023-05-04 09:17:01 +0530

    hiii

---
+++ CaseToolsExam/src/Calculator_220.java
@@ -0,0 +1,16 @@
+
+public class Calculator_220 {
+	int num1,num2;
+	Calculator_220(int num1, int num2)
+	{
+		this.num1=num1;
+		this.num2=num2;
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
